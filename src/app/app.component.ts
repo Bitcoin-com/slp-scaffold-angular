570b4b9d99fc754ed14cdf55ef2f1676aaf54b02
Gabriel Cardona <gabriel@bitcoin.com>
2018-10-26 12:10:23 +0200

    list all tokens.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,11 @@
-import { Component } from '@angular/core';
-let Wormhole = require('wormholecash/lib/Wormhole').default;
-let wormhole = new Wormhole();
+import { Component } from "@angular/core";
+let SLPSDK = require("slp-sdk/lib/SLP").default;
+let SLP = new SLPSDK();
+(async () => {
+  try {
+    let list = await SLP.list();
+    console.log(list);
+  } catch (error) {
+    console.error(error);
+  }
+})();
